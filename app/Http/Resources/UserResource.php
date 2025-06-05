f99a12e5ab62f2f45b6d5ad43b6479a333a52d04
Mostafa <mstf.mbz@gmail.com>
2025-06-05 14:16:58 +0330

    fix: updating profile

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -21,7 +21,7 @@ class UserResource extends JsonResource
             'phone_number' => $this->phone_number,
             'avatar_url' => $this->avatar_url,
             'gender' => $this->gender,
-            'birthday' => $this->birthday?->format('Y-m-d'),
+            'birthday' => $this->birthday ? (is_string($this->birthday) ? $this->birthday : $this->birthday->format('Y-m-d')) : null,
             'bio' => $this->bio,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'activities_count' => $this->whenCounted('activities'),
